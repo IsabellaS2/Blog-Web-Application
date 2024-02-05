a1e1f8c0f9417d69800b3b78b3282c3edc026ba3
IsabellaSulisufi <114592322+IsabellaSulisufi@users.noreply.github.com>
2024-02-05 16:27:32 +0000

    Adding entry page for user to add name and saving it

--- app.js
+++ app.js
@@ -1,14 +1,38 @@
 import express from "express";
 import bodyParser from "body-parser";
+import session from "express-session";
 
 const app = express();
 const port = 3000;
-app.use(express.static("public"))
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: "apples",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+app.get("/entry-page", (req, res) => {
+  res.render("entryPage.ejs");
+});
 
 app.get("/", (req, res) => {
-   res.render("index.ejs")
- });
+  // Retrieve the user's name from the session
+  const userName = req.session.userName;
+  res.render("home.ejs", { userName });
+});
 
- app.listen(port, () => {
-   console.log(`Listening on port ${port}`);
- });
+app.post("/save-name", (req, res) => {
+  const userName = req.body.name;
+  // Save the user's name in the session
+  req.session.userName = userName;
+  // Redirect to another page (e.g., homepage)
+  res.redirect("/");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
